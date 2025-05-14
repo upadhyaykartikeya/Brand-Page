6c95a2bed84ba9a3737c4060ecc21a029e09277f
upadhyaykartikeya <ukartik451@gmail.com>
2025-05-14 09:55:19 +0530

    Adding Project

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+
+const Navigation = () => {
+    return (
+    <nav className="container">
+      <div className="Logo">
+        <img src="/images/brand_logo.png" alt="logo"/>
+      </div>
+
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
